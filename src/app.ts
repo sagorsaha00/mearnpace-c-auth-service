158b3c814ee5bc9e805fa7cb8ff5aefe907a6d9d
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2024-11-27 22:00:13 +0600

    ago code app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
-import express from 'express';
-const app = express();
+import { HttpError } from 'http-errors'
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import express, { NextFunction, Request, Response } from 'express'
+import logger from './config/logger'
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
- 
-export default app;
+const app = express()
+app.get('/', (req, res) => {
+   res.send('Hello World!')
+})
+
+//global error-handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+   logger.error(err.message)
+   const statuscode = err.statusCode || 500
+
+   res.status(statuscode).json({
+      errors: [
+         {
+            type: err.name,
+            message: err.message,
+            path: '',
+         },
+      ],
+   })
+})
+export default app
