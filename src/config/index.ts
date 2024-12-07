b937692bdcdff3ea9dfe7200c3dfccfba5e0c115
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2024-12-07 22:54:56 +0600

    ago index js

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,18 @@
-import { config } from "dotenv";
-config();
 
-const PORT = process.env.PORT;
-const NODE_ENV = process.env.NODE_ENV
+import path from "node:path";
+import * as dotenv from "dotenv";
+ 
+
+ const {PORT,NODE_ENV,DB_HOST,DB_PORT,DB_PASSWORD,DB_USERNAME,DB_NAME } = process.env
+
+// config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+dotenv.config({
+    path: path.join(__dirname, `../../.env.${process.env.NODE_ENV || "dev"}`),
+});
 
 export const Config = {
     PORT,
-    NODE_ENV
+    NODE_ENV,
+    DB_HOST,DB_PORT,DB_PASSWORD,DB_USERNAME,DB_NAME
 }
 
