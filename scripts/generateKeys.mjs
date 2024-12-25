8d2b04c6e89d5d72ce07a9b84e010dbbe211b7d6
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2024-12-25 19:48:14 +0600

    add key genaration script

--- scripts/generateKeys.mjs
+++ scripts/generateKeys.mjs
@@ -1,13 +1,11 @@
 import crypto, { generateKeyPairSync } from 'crypto'
 import fs from 'fs'
 
-
 const { privateKey, publicKey } = generateKeyPairSync('rsa', {
    modulusLength: 2048,
    publicKeyEncoding: { type: 'spki', format: 'pem' },
    privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
 })
-
 
 console.log('privateKey', privateKey)
 console.log('publicKey', publicKey)
