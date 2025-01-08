d0a310da8363536f34650b496f7d847133c128fc
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2025-01-08 19:19:18 +0600

    genrate a public key

--- scripts/convertPemToJwk.mjs
+++ scripts/convertPemToJwk.mjs
@@ -1,15 +1,15 @@
-import pkg from 'node-jose';
-const { JWK } = pkg;
+import pkg from 'node-jose'
+const { JWK } = pkg
 import fs from 'fs'
-import { json } from 'stream/consumers';
+import { json } from 'stream/consumers'
 
-const privateKey =  fs.readFileSync('certs/privateKey.pem', 'utf8');
+const privateKey = fs.readFileSync('certs/privateKey.pem', 'utf8')
 
 async function convertToJWK() {
-  const keystore = JWK.createKeyStore();
-  const key = await keystore.add(privateKey, 'pem');
- 
-  console.log(  JSON.stringify(key) );
+   const keystore = JWK.createKeyStore()
+   const key = await keystore.add(privateKey, 'pem')
+
+   console.log(JSON.stringify(key))
 }
 
-convertToJWK().catch(console.error);
+convertToJWK().catch(console.error)
