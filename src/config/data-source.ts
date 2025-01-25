62c14118d126068c2a062ad0155ffdc753375fbc
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2025-01-25 23:07:14 +0600

    fix js or ts file support

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -15,7 +15,7 @@ export const AppDataSource = new DataSource({
    //dont-touch in production alawaya keep false
    synchronize: false,
    logging: false,
-   entities: ['src/entity/*.ts'],
-   migrations: ['src/migration/*.ts'],
+   entities: ['src/entity/*.{ts,js}'],
+   migrations: ['src/migration/*.{ts,js}'],
    subscribers: [],
 })
