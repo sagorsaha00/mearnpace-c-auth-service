9d101567891e2382c4be3a57f04eb808d7e892d3
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2025-02-04 19:18:32 +0600

    try to fix

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,25 +1,3 @@
-import { RequestHandler } from 'express'
-import path from 'node:path'
-// import 'reflect-metadata'
-// import { DataSource } from 'typeorm'
-// import { Config } from '.'
-
-// console.log("DB_PASSWORD:", Config.DB_PASSWORD)
-// export const AppDataSource = new DataSource({
-//    type: 'postgres',
-//    host: Config.DB_HOST,
-//    port: Number(Config.DB_PORT),
-//    username: Config.DB_USERNAME,
-//    password: (Config.DB_PASSWORD),
-//    database: Config.DB_NAME,
-//    synchronize: false,
-//    logging: Config.NODE_ENV === 'development',
-//    entities: [path.join(__dirname, '../entity/*.{js,ts}')],
-//    migrations: [path.join(__dirname, '../migration/*.{js,ts}')],
-//    subscribers: [],
-
-// })
-
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 import { Config } from '.'
@@ -27,7 +5,7 @@ import { Config } from '.'
 export const AppDataSource = new DataSource({
    type: 'postgres',
    host: Config.DB_HOST,
-   port: Number(Config.DB_PORT),
+   port: 5432,
    username: Config.DB_USERNAME,
    password: Config.DB_PASSWORD,
    database: Config.DB_NAME,
